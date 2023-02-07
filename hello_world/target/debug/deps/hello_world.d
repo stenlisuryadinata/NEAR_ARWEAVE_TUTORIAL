e0a22b0650c5b530c0774776ae1f35daa9bfd3c8
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2023-02-08 02:48:02 +0700

    Delete hello_world.d

--- hello_world/target/debug/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\users\dell\documents\NEAR-Arweave-Tutorial\NEAR-Arweave-Tutorial\hello_world\target\debug\deps\hello_world.exe: src\main.rs
-
-C:\users\dell\documents\NEAR-Arweave-Tutorial\NEAR-Arweave-Tutorial\hello_world\target\debug\deps\hello_world.d: src\main.rs
-
-src\main.rs:

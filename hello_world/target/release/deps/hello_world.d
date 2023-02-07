40f581dad16f8fff7d8927e1969f8c57c905549a
Stenli Suryadinata <47641964+stenlisuryadinata@users.noreply.github.com>
2023-02-08 02:49:22 +0700

    Delete hello_world.d

--- hello_world/target/release/deps/hello_world.d
+++
@@ -1,5 +0,0 @@
-C:\users\dell\documents\NEAR-Arweave-Tutorial\NEAR-Arweave-Tutorial\hello_world\target\release\deps\hello_world.exe: src\main.rs
-
-C:\users\dell\documents\NEAR-Arweave-Tutorial\NEAR-Arweave-Tutorial\hello_world\target\release\deps\hello_world.d: src\main.rs
-
-src\main.rs:
